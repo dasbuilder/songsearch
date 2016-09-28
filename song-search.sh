1521cabe0d5a4dab513af66fe4cbf7c19b9dafa3
dasbuilder <seanderson88@gmail.com>
2016-09-28 02:18:20 -0500

    Update song-search.sh

--- song-search.sh
+++ song-search.sh
@@ -2,5 +2,5 @@
 IFS="$(printf '\n')";
 echo 'Input name of artist or song title you want to search: ';
 read -e tittle;
-output=$(find /root/testing/plugdj -type f -exec grep -ioHP '[^"\{\}]*"title":"([^"]+)"' {} \; | grep $tittle | sed 's/\.json//g' | tr ':,' ' ' | cut -d\/ -f5 | awk -F\" '{ print $1,$4}')
+output=$(find /insert/path/here -type f -exec grep -ioHP '[^"\{\}]*"title":"([^"]+)"' {} \; | grep $tittle | sed 's/\.json//g' | tr ':,' ' ' | cut -d\/ -f5 | awk -F\" '{ print $1,$4}')
 echo $output;
