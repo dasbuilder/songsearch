ec4856e2fe1029b7559d4141b2e20e0411cadca0
dasbuilder <seanderson88@gmail.com>
2018-02-27 18:20:19 -0600

    Update dubtrack_jq.sh
    Updated some code to fix minor bugs. You will also need to replace field 6 in cut on line 29 so that it reflects your full path to your file on line 21.

--- dubtrack_jq.sh
+++ dubtrack_jq.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 ################
-##  V. 1.0.0  ##
+##  V. 1.0.2  ##
 ## by Spencer ##
 ################
 # This function allows one to search all of their Dubtrack playlists after downloading them from DT using JTBrinkmann's script, found here: https://github.com/JTBrinkmann/Dubtrack-Playlist-Pusher/
 
-dtPlaylistChk(){ 
+dtPlaylistChk(){
    # Setting the user-agent, change this to your liking
-   ua="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36";    
-   echo "What artist or song title to search?"; 
+   ua="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36";
+   echo "What artist or song title to search?";
    read -r songinfo;
    until [[ -n "$songinfo" ]]; do
-     if [[ -z "$songinfo" ]]; then 
-       echo "You need to enter a song name or artist"; 
+     if [[ -z "$songinfo" ]]; then
+       echo "You need to enter a song name or artist";
        read -r songinfo;
-     fi; 
+     fi;
    done;
-   # cleanly get output from all .json files with % as the delimiter. 
+   # cleanly get output from all .json files with % as the delimiter.
    # Change % to your preferred delimiter both here and in the while loop below
-   jq -r '.data[]|"\(input_filename)%\(.image)%\(.title)"' *.json | 
-   
+   jq -r '.data[]|"\(input_filename)%\(.image)%\(.title)"' /home/USERn/testing/plugdj/*.json |
+
    # Grep for the artist or song title and replace any translated characters
-   grep -i "${songinfo}" | 
-   gsed -r "s_(&amp;)_\\&_g; s_(&apos;)_\\'_g; s_(&quot;)_\\"_g" | 
+   grep -i "${songinfo}" |
+   gsed -r "s_(&amp;)_\\&_g; s_(&apos;)_\\'_g; s_(&quot;)_\"_g" |
    # Loop to take data from jq and neatly print
-   while IFS='%' read -r filename responses titles; do 
-      printf "%s\n%s %s\n\v" \
-             "$filename" \
-             "$(curl -sIL "$responses" -A "$ua" | awk '/HTTP/ { print $2 }')" "$titles"; 
-             sleep .5; 
-    done;
- }
+   while IFS='%' read -r filename responses titles; do
+      printf "%s\n%s %s\n\n" \
+             "$(echo "$filename" | cut -d\/ -f6-)" \
+             "$(curl -sIL "$responses" -A "$ua" | awk '/HTTP/ { print $2 }')" \
+             "$titles";
+             sleep .5;
+   done;
+ };
 
 dtPlaylistChk
